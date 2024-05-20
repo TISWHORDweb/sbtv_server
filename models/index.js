b11c424662fdf6918776ea3e84ed2f54476051a6
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-05-20 20:28:48 +0100

    Models relationship

--- models/index.js
+++ models/index.js
@@ -3,3 +3,9 @@ exports.ModelUser = require('./model.user');
 exports.ModelVideo = require('./model.video');
 exports.ModelAdmin= require('./model.admin');
 exports.ModelTalent= require('./model.talent');
+
+
+require('./model.talent').belongsTo(require("./model.user"), {
+    as: 'user',
+    foreignKey: {name: 'uid', allowNull: null}
+});
