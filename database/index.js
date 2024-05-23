d448d87da3008efef4f83cec186fb3b23d4a5192
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-05-23 12:18:03 +0100

    Database update

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const MODE = process.env.MODE === "production";
 /**
  * @type {string} default server uri
  */
-const DATABASE_HOST = "localhost";
+const DATABASE_HOST = MODE ? process.env.DB_HOST : process.env.DEBUG_DB_HOST;
 const DATABASE_LANG = "mysql";
 /**
  * @type {string} database common name
@@ -18,6 +18,11 @@ const DATABASE_USER = MODE ? process.env.DB_USER : process.env.DEBUG_DB_USER;
  */
 const DATABASE_PASS = MODE ? process.env.DB_PASS : process.env.DEBUG_DB_PASS;
 /**
+ * @type {number} database common port
+ */
+const DATABASE_PORT = MODE ? process.env.DB_PORT : process.env.DEBUG_DB_PORT;
+
+/**
  * Call for initialization
  */
 const {Sequelize} = require('sequelize');
@@ -28,9 +33,11 @@ const {Sequelize} = require('sequelize');
 const dbConn = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASS, {
     host: DATABASE_HOST,
     dialect: DATABASE_LANG,
+    port: DATABASE_PORT,
     logging: (e) => {
         //write to log file here...
         // console.log(e);
+        // console.log("connected");
     },
 });
 module.exports = dbConn;
